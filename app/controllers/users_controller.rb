a64e12e16fe876d69a95a11af9a58a734ac46258
Jefferson Choi <jeffersonchoi@gmail.com>
2015-07-03 00:33:58 -0700

    add user show, edit, and update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,10 @@ class UsersController < ApplicationController
      @users = User.all
    end
 
+   def show
+     @user = User.find(params[:id])
+   end
+
    def new
      @user = User.new
    end
@@ -16,6 +20,20 @@ class UsersController < ApplicationController
      else
        render :new
      end
+   end
+
+   def edit
+    @user = User.find(current_user)
+   end
+
+   def update
+    @user = User.find(current_user)
+
+    if @user.update_attributes(user_params)
+      redirect_to users_path
+    else
+      render :edit
+    end
    end
 
 private
