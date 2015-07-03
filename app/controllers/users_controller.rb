df112c208a1addec92b1333fbc590c225b74fb67
Jefferson Choi <jeffersonchoi@gmail.com>
2015-07-02 19:35:31 -0700

    edit user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,26 @@
 class UsersController < ApplicationController
 
   def index
-    @user = User.all
-  end
+     @users = User.all
+   end
 
+   def new
+     @user = User.new
+   end
+
+   def create
+     @user = User.new(user_params)
+     if @user.save
+       redirect_to users_path
+     else
+       render :new
+     end
+   end
+
+   private
+
+   def user_params
+     params.require(:user).permit(:first_name, :last_name, :gender, :email, :password, :password_confirmation, :date_of_birth)
+   end
 
 end
