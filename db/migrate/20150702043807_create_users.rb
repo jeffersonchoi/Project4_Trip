44661477120cdb1bb4274c3d5ef7a3ed0b65e914
Jefferson Choi <jeffersonchoi@gmail.com>
2015-07-02 19:24:36 -0700

    create user controller

--- db/migrate/20150702043807_create_users.rb
+++ db/migrate/20150702043807_create_users.rb
@@ -3,6 +3,7 @@ class CreateUsers < ActiveRecord::Migration
     create_table :users do |t|
       t.string :first_name
       t.string :last_name
+      t.string :gender
       t.string :email
       t.string :password_digest
       t.date :date_of_birth
